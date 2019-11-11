de7e2f3e016fdd7a7552620476aa6b317c9ec5d9
sbx_user1051 <sbx_user1051@169.254.126.149>
2019-11-11 19:48:15 +0000

    Mon Nov 11 19:48:15 UTC 2019

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -12,12 +12,15 @@ def compute(x, y):
 def _check_type(obj, type, range):
     """
     Function to check the type of object and if the object is betwwen a range:
-    obj (obj)
+    obj (obj): the pobject to be checked
+    type: the type of the object
+    range: (list): the range (a list with 2 elements; min and max) of the which the number should be checked; eg: [0,1]
 
     """
     if isinstance(obj,type) and (range[0] <= obj <= range[1]) :
         return True
     else:
         raise ValueError("The {obj} is not of type {type} or is not between {range}".format(obj=obj,type=type,range=range))
+
 
 
