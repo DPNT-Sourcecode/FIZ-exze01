70f4ff440e23ad82fa5d0779ce14a74be081f742
sbx_user1051 <sbx_user1051@169.254.2.149>
2019-11-11 18:44:16 +0000

    Mon Nov 11 18:44:16 UTC 2019

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,7 +1,12 @@
 # noinspection PyShadowingBuiltins,PyUnusedLocal
 def compute(x, y):
-    if isinstance(x,int) 
+    if isinstance(x,int) and x  
     raise NotImplementedError()
 
+
+def _check_type(obj, type):
+    """
+    check if an ob
+    """
 
 
