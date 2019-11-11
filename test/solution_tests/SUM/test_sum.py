5a0043bf95d82927f1da385f8641b6d9bd0a32df
sbx_user1051 <sbx_user1051@169.254.126.149>
2019-11-11 19:42:15 +0000

    Mon Nov 11 19:42:15 UTC 2019

--- test/solution_tests/SUM/test_sum.py
+++ test/solution_tests/SUM/test_sum.py
@@ -6,15 +6,22 @@ class TestSum():
         # test sum function return expected putput
         assert sum_solution.compute(1, 2) == 3
     
-    def test_sum_negative_numbers(self):
-        # test sun function raise error when the number is negative
+    def test_sum_raise_error_when_negative_numbers(self):
+        # test sun function raise error when the number is not integer positive
         with pytest.raises(ValueError):
             sum_solution.compute(-1,7)
+            
+    def test_sum_raise_erro_when_adds_floats(self):
+        # test sun function raise error when the numbers are floats
+        with pytest.raises(ValueError):
+            sum_solution.compute(1.5,5.9)
 
     def test_sum_number_higher_than_100(self):
+        
         with pytest.raises(ValueError):
             sum_solution.compute(200,5)
         
+
 
 
 
