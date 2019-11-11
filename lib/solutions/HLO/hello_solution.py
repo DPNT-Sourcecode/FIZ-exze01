854c1c6f55334673e0f1db50b0c7513287e31743
sbx_user1051 <sbx_user1051@169.254.126.149>
2019-11-11 19:59:50 +0000

    Mon Nov 11 19:59:50 UTC 2019

--- lib/solutions/HLO/hello_solution.py
+++ lib/solutions/HLO/hello_solution.py
@@ -2,6 +2,8 @@
 
 # noinspection PyUnusedLocal
 # friend_name = unicode string
-def hello(friend_name):
-    return f"hello {}"
+def hello(friend_name=None):
+    if isinstance
+    return f"hello world"
+
 
