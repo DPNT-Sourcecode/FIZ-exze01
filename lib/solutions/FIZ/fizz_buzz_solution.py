ba226daa039db741a96e2068cc22f7912bd9f9b5
sbx_user1051 <sbx_user1051@169.254.126.149>
2019-11-11 21:39:22 +0000

    Mon Nov 11 21:39:22 UTC 2019

--- lib/solutions/FIZ/fizz_buzz_solution.py
+++ lib/solutions/FIZ/fizz_buzz_solution.py
@@ -2,24 +2,24 @@
 def fizz_buzz(number):
     if not (1 <= number <= 9999):
         raise ValueError("NUmber not between 1 and 9999")
-    if number > 10 and len(set(str(number))) == 1 and (_check_multiple_of(number,3) or ('3' in str(number))) and (_check_multiple_of(number,5) or ('5' in str(number))) :
+    if _c and (_check_multiple_of(number,3) or ('3' in str(number))) and (_check_multiple_of(number,5) or ('5' in str(number))) :
         if _check_multiple_of(number,2):
             return  "fizz buzz deluxe" 
         else:
             return "fizz buzz fake deluxe"  
-    elif (_check_multiple_of(number,3) or ('3' in str(number))) and (_check_multiple_of(number,5) or ('5' in str(number))):
+    elif _check_buzz(number) and _check_fizz(number):
         return  "fizz buzz" 
-    elif number > 10 and len(set(str(number))) == 1  and ( _check_multiple_of(number,3) or ('3' in str(number))):
+    elif _check_deluxe(number)  and _check_fizz(number):
         if _check_multiple_of(number,2):
             return  "fizz deluxe"
         else:
             return "fizz fake deluxe"
-    elif number > 10 and len(set(str(number))) == 1  and ( _check_multiple_of(number,5) or ('5' in str(number))):
+    elif _check_deluxe(number)  and _check_buzz(number):
         if _check_multiple_of(number,2):
             return  "buzz deluxe"
         else:
             return  "buzz fake deluxe"
-    elif number > 10 and len(set(str(number))) == 1:
+    elif _check_deluxe(number):
         if _check_multiple_of(number,2): 
             return "deluxe"
         else:
@@ -42,5 +42,9 @@ def _check_fizz(number):
 
 def _check_buzz(number):
     if  _check_multiple_of(number,5) or ('5' in str(number)):
+        return True
+
+def _check_deluxe(number):
+    if number > 10 and len(set(str(number))) == 1:
         return True
 
