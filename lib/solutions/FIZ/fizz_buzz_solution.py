4a41620adc2f0ef1c64904ebbe950c685d142ac2
sbx_user1051 <sbx_user1051@169.254.126.149>
2019-11-11 20:59:45 +0000

    Mon Nov 11 20:59:45 UTC 2019

--- lib/solutions/FIZ/fizz_buzz_solution.py
+++ lib/solutions/FIZ/fizz_buzz_solution.py
@@ -4,6 +4,8 @@ def fizz_buzz(number):
         raise ValueError("NUmber not between 1 and 9999")
     if (_check_multiple_of(number,3) or ('3' in str(number))) and (_check_multiple_of(number,5) or ('5' in str(number))) and (number > 10 and len(set(str(number)))):
             return  "fizz buzz deluxe"  
+    elif (_check_multiple_of(number,3) or ('3' in str(number))) and (_check_multiple_of(number,5) or ('5' in str(number))):
+            return  "fizz buzz"  
     elif number > 10 and len(set(str(number))) == 1:
         return "deluxe"   
     elif _check_multiple_of(number,3) or ('3' in str(number)):
@@ -18,5 +20,6 @@ def fizz_buzz(number):
 def _check_multiple_of(n, k):
     if n % k == 0:
         return True
+
 
 
--- test/solution_tests/FIZ/test_fiz.py
+++ test/solution_tests/FIZ/test_fiz.py
@@ -6,9 +6,9 @@ class TestSum():
         # test sum function return expected putput
         assert fizz_buzz_solution.fizz_buzz(3) == "fizz"
         assert fizz_buzz_solution.fizz_buzz(5) == "buzz"
-       # assert fizz_buzz_solution.fizz_buzz(30) == "fizz buzz"
+        assert fizz_buzz_solution.fizz_buzz(30) == "fizz buzz"
         assert fizz_buzz_solution.fizz_buzz(2) == 2
-        # assert fizz_buzz_solution.fizz_buzz(15) == "fizz buzz"
+        assert fizz_buzz_solution.fizz_buzz(15) == "fizz buzz"
         assert fizz_buzz_solution.fizz_buzz(37) == 'fizz'
         assert fizz_buzz_solution.fizz_buzz(50) == 'buzz'
         assert fizz_buzz_solution.fizz_buzz(555) == "fizz buzz deluxe"
@@ -17,3 +17,4 @@ class TestSum():
         assert fizz_buzz_solution.fizz_buzz(99) == "deluxe"
     
     
+
