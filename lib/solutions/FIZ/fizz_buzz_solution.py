ed76c56bd12a7144a7261d5733b4ca4c9be53426
sbx_user1051 <sbx_user1051@169.254.126.149>
2019-11-11 20:12:50 +0000

    Mon Nov 11 20:12:50 UTC 2019

--- lib/solutions/FIZ/fizz_buzz_solution.py
+++ lib/solutions/FIZ/fizz_buzz_solution.py
@@ -1,5 +1,14 @@
 # noinspection PyUnusedLocal
 def fizz_buzz(number):
-    if 
+    if _check_multiple_of(number,3):
+        return "fizz"
+    elif _check_multiple_of(number,5):
+        return 
     raise NotImplementedError()
+
+
+def _check_multiple_of(n, k):
+    if n % k == 0:
+        return True
+
 
