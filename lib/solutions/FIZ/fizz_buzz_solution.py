df8dfe313840a3c50d96ac3965326bfb4a2e8028
sbx_user1051 <sbx_user1051@169.254.126.149>
2019-11-11 20:15:50 +0000

    Mon Nov 11 20:15:50 UTC 2019

--- lib/solutions/FIZ/fizz_buzz_solution.py
+++ lib/solutions/FIZ/fizz_buzz_solution.py
@@ -1,14 +1,20 @@
 # noinspection PyUnusedLocal
 def fizz_buzz(number):
+    if  1 <= number <= 9999:
+        raise ValueError("NUmber not between 1 and 9999")
     if _check_multiple_of(number,3):
         return "fizz"
     elif _check_multiple_of(number,5):
-        return 
-    raise NotImplementedError()
-
+        return "buzz"
+    elif _check_multiple_of(number,15):
+        return  "fizz buzz"
+    else:
+        return str(number)
+   
 
 def _check_multiple_of(n, k):
     if n % k == 0:
         return True
+
 
 
