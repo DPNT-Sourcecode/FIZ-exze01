b6fc076097397fd80bd5348fd4e5536daff06189
sbx_user1051 <sbx_user1051@169.254.126.149>
2019-11-11 21:51:22 +0000

    Mon Nov 11 21:51:22 UTC 2019

--- test/solution_tests/FIZ/test_fiz.py
+++ test/solution_tests/FIZ/test_fiz.py
@@ -4,22 +4,20 @@ import pytest
 class TestSum():
     def test_fiz(self):
         # test sum function return expected putput
-        assert fizz_buzz_solution.fizz_buzz(3) == "fiz fake deluxe"
-        assert fizz_buzz_solution.fizz_buzz(5) == "fake deluxe"
-        assert fizz_buzz_solution.fizz_buzz(30) == "fizz buzz"
+        assert fizz_buzz_solution.fizz_buzz(3) == "fizz fake deluxe"
+        assert fizz_buzz_solution.fizz_buzz(5) == "buzz fake deluxe"
+        assert fizz_buzz_solution.fizz_buzz(30) == "fizz buzz deluxe"
         assert fizz_buzz_solution.fizz_buzz(2) == 2
-        assert fizz_buzz_solution.fizz_buzz(15) == "fizz buzz"
+        assert fizz_buzz_solution.fizz_buzz(15) == "fizz buzz fake deluxe"
         assert fizz_buzz_solution.fizz_buzz(37) == 'fizz'
-        assert fizz_buzz_solution.fizz_buzz(50) == 'buzz'
+        assert fizz_buzz_solution.fizz_buzz(50) == 'buzz deluxe'
         assert fizz_buzz_solution.fizz_buzz(555) == "fizz buzz fake deluxe"
         assert fizz_buzz_solution.fizz_buzz(546) == "fizz buzz"
-        assert fizz_buzz_solution.fizz_buzz(22) == "deluxe"
-        assert fizz_buzz_solution.fizz_buzz(11) == "fake deluxe"
-        assert fizz_buzz_solution.fizz_buzz(77) == "fake deluxe"
-        assert fizz_buzz_solution.fizz_buzz(111) == "fizz fake deluxe"
-        assert fizz_buzz_solution.fizz_buzz(99) == "fizz fake deluxe"
+        assert fizz_buzz_solution.fizz_buzz(22) == 22
+        assert fizz_buzz_solution.fizz_buzz(111) == "fizz"
+        assert fizz_buzz_solution.fizz_buzz(99) == "fizz"
         assert fizz_buzz_solution.fizz_buzz(33) == "fizz fake deluxe"
-        assert fizz_buzz_solution.fizz_buzz(222) == "fizz deluxe"
+        assert fizz_buzz_solution.fizz_buzz(222) == "fizz"
         assert fizz_buzz_solution.fizz_buzz(777) == "fizz fake deluxe"
     
     
