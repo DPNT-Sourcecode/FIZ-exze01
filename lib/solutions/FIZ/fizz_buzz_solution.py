1c11a6af98d5f0bcdc88c970509aff757666ca74
sbx_user1051 <sbx_user1051@169.254.126.149>
2019-11-11 21:02:45 +0000

    Mon Nov 11 21:02:45 UTC 2019

--- lib/solutions/FIZ/fizz_buzz_solution.py
+++ lib/solutions/FIZ/fizz_buzz_solution.py
@@ -2,7 +2,7 @@
 def fizz_buzz(number):
     if not (1 <= number <= 9999):
         raise ValueError("NUmber not between 1 and 9999")
-    if (_check_multiple_of(number,3) or ('3' in str(number))) and (_check_multiple_of(number,5) or ('5' in str(number))) and (number > 10 and len(set(str(number)))):
+    if number > 10 and len(set(str(number))) and (_check_multiple_of(number,3) or ('3' in str(number))) and (_check_multiple_of(number,5) or ('5' in str(number))) :
             return  "fizz buzz deluxe"  
     elif (_check_multiple_of(number,3) or ('3' in str(number))) and (_check_multiple_of(number,5) or ('5' in str(number))):
             return  "fizz buzz"  
@@ -20,6 +20,7 @@ def fizz_buzz(number):
 def _check_multiple_of(n, k):
     if n % k == 0:
         return True
+
 
 
 
