90ed8829ae5a37d7433f6f3fed63dfa590a15a23
sbx_user1051 <sbx_user1051@169.254.126.149>
2019-11-11 20:06:00 +0000

    Mon Nov 11 20:06:00 UTC 2019

--- lib/solutions/HLO/hello_solution.py
+++ lib/solutions/HLO/hello_solution.py
@@ -6,9 +6,4 @@ def hello(friend_name):
     if isinstance(friend_name,str):
         return "Hello, {friend_name}!".format(friend_name=friend_name)
     else:
-        raise ValueError("give a name")
-
-
-
-
-
+        raise ValueError("the argument is not string")
