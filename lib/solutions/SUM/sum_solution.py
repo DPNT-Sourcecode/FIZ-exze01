a416504c8b74cd2cbd76bc11bb817ce4625305ba
sbx_user1051 <sbx_user1051@169.254.2.149>
2019-11-11 18:50:16 +0000

    Mon Nov 11 18:50:16 UTC 2019

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,14 +1,10 @@
 # noinspection PyShadowingBuiltins,PyUnusedLocal
 def compute(x, y):
-    if isinstance(x,int) and x  
-    raise NotImplementedError()
-
+    if _check_type(x,int,[0,100]) and _check_typ(y,int,[0,100])
+        return x + y
 
 def _check_type(obj, type, range):
-    if isinstance(obj,type) and (range[0] <= obj <= 100) :
+    if isinstance(obj,type) and (range[0] <= obj <= range[1]) :
         return True
     else:
-        raise ValueError("The {obj} is not of type {type} or is not between ")
-
-
-
+        raise ValueError("The {obj} is not of type {type} or is not between {range}".format(obj=obj,type=type,range=range))
