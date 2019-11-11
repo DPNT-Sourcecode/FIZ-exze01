0240909d4082836fbe6d94b935e9ecaca74aed95
sbx_user1051 <sbx_user1051@169.254.126.149>
2019-11-11 21:08:30 +0000

    Mon Nov 11 21:08:30 UTC 2019

--- lib/solutions/FIZ/fizz_buzz_solution.py
+++ lib/solutions/FIZ/fizz_buzz_solution.py
@@ -2,18 +2,16 @@
 def fizz_buzz(number):
     if not (1 <= number <= 9999):
         raise ValueError("NUmber not between 1 and 9999")
-    if (_check_multiple_of(number,3) or ('3' in str(number))) and (_check_multiple_of(number,5) or ('5' in str(number))):
-        return  "fizz buzz"  
-    elif number > 10 and len(set(str(number))) and (_check_multiple_of(number,3) or ('3' in str(number))) and (_check_multiple_of(number,5) or ('5' in str(number))) :
-        return  "fizz buzz deluxe"  
-   
+    if number > 10 and len(set(str(number))) == 1 and (_check_multiple_of(number,3) or ('3' in str(number))) and (_check_multiple_of(number,5) or ('5' in str(number))) :
+        return  "fizz buzz deluxe"   
+    elif (_check_multiple_of(number,3) or ('3' in str(number))) and (_check_multiple_of(number,5) or ('5' in str(number))):
+        return  "fizz buzz"     
     elif number > 10 and len(set(str(number))) == 1:
         return "deluxe"   
     elif _check_multiple_of(number,3) or ('3' in str(number)):
         return "fizz"
     elif _check_multiple_of(number,5) or ('5' in str(number)):
         return "buzz"
-    
     else:
         return number
    
@@ -21,6 +19,7 @@ def fizz_buzz(number):
 def _check_multiple_of(n, k):
     if n % k == 0:
         return True
+
 
 
 
