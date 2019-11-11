b43a9f47dcd84f3f793b36c3cc520ca745db1042
sbx_user1051 <sbx_user1051@169.254.126.149>
2019-11-11 19:39:15 +0000

    Mon Nov 11 19:39:15 UTC 2019

--- test/solution_tests/SUM/test_sum.py
+++ test/solution_tests/SUM/test_sum.py
@@ -1,12 +1,20 @@
 
 from lib.solutions.SUM import sum_solution
-
+import pytest
 class TestSum():
     def test_sum(self):
+        # test sum function return expected putput
         assert sum_solution.compute(1, 2) == 3
     
-    def test_sum_negative(self):
-        asser
+    def test_sum_negative_numbers(self):
+        # test sun function raise error when the number is negative
+        with pytest.raises(ValueError):
+            sum_solution.compute(-1,7)
+
+    def test_sum_number_higher_than_100(self):
+        with pytest.raises(ValueError):
+            sum_solution.compute(200,5)
         
+
 
 
